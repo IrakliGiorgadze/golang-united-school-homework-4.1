c0d6d9253d6557ec82c011ce1e30cb8fbeb00f46
IrakliGiorgadze <ika.giorgadze@gmail.com>
2022-05-12 18:12:24 +0400

    please fix tests from your side

--- reverse_string.go
+++ reverse_string.go
@@ -1,11 +1,8 @@
 package reverse_string
 
-import "fmt"
-
 func ReverseString(input string) (output string) {
 	for _, v := range input {
 		output = string(v) + output
 	}
-	fmt.Println(output)
 	return output
 }
